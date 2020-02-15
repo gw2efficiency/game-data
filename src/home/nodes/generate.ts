86937ec173f1d81c4273168150b92738fb26a73c
queicherius <queicherius@gmail.com>
2020-02-15 15:12:00 +0000

    Add new home instance nodes

--- src/home/nodes/generate.ts
+++ src/home/nodes/generate.ts
@@ -29,7 +29,7 @@ async function run () {
         `  { `,
         `id: '${node.id}', `,
         `name: '${escapeQuotes(previousData.name)}', `,
-        `unlock_items: ${JSON.stringify(previousData.unlock_items)}`,
+        `unlock_items: ${JSON.stringify(previousData.unlock_items).replace(',', ', ')}`,
         ` }`
       ].join('')
     })
