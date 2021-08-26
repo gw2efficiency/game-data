6b911cb2edad9df273cc5da98125d1eb431b455c
queicherius <queicherius@gmail.com>
2021-08-26 19:34:35 +0000

    v0.1.26

--- publish-data.sh
+++ publish-data.sh
@@ -2,8 +2,19 @@
 set -e
 
 rm -rf build/
+echo ">> Cleaned up build"
+
 yarn version --patch
+echo ">> Bumped version"
+
 yarn build
+echo ">> Built"
+
 cd build/ || exit 1
+echo ">> Switched into build"
+
 yarn publish --access=public
+echo ">> Published"
+
 git push
+echo ">> Pushed"
