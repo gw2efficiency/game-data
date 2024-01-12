3f052a1e404828b32affa263526a55f9f7df101c
Ecmelt <satanistanbul@hotmail.com>
2024-01-12 14:04:51 +0000

    Aurene Legendary Variants

---
+++ src/skins/categories/aurene-legendary-variants/data.ts
@@ -0,0 +1,34 @@
+export interface GameDataSkinsCategoriesAureneLegendaryVariants {
+  /** The grouping key for the aura */
+  variant_key: string
+
+  /** The item id */
+  id: number
+
+  /** The item name */
+  name: string
+}
+
+// prettier-ignore
+const GAME_DATA_SKINS_CATEGORIES_AURENE_LEGENDARY_VARIANTS: Array<GameDataSkinsCategoriesAureneLegendaryVariants> = [
+  // -- GENERATE-START --
+  { variant_key: 'zhaitanVariantSkins', id: 98091, name: 'Zhaitan\'s Rending Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98098, name: 'Zhaitan\'s Tail Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98099, name: 'Zhaitan\'s Persuasion Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98104, name: 'Zhaitan\'s Bite Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98109, name: 'Zhaitan\'s Claw Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98116, name: 'Zhaitan\'s Scale Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98118, name: 'Zhaitan\'s Argument Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98120, name: 'Zhaitan\'s Insight Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98138, name: 'Zhaitan\'s Weight Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98142, name: 'Zhaitan\'s Flight Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98143, name: 'Zhaitan\'s Gaze Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98144, name: 'Zhaitan\'s Breath Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98146, name: 'Zhaitan\'s Fang Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98158, name: 'Zhaitan\'s Voice Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98160, name: 'Zhaitan\'s Wisdom Skin' },
+  { variant_key: 'zhaitanVariantSkins', id: 98162, name: 'Zhaitan\'s Wing Skin' },
+  // -- GENERATE-END --
+]
+
+export default GAME_DATA_SKINS_CATEGORIES_AURENE_LEGENDARY_VARIANTS
---
+++ src/skins/categories/aurene-legendary-variants/generate.ts
@@ -0,0 +1,73 @@
+import * as path from 'path'
+import * as fs from 'fs'
+import fetch from 'node-fetch'
+import escapeRegex from 'escape-regex-string'
+import { escapeQuotes } from '../../../_helpers/generate'
+
+interface VariantSkinName {
+  variantKey: string
+  match: RegExp
+}
+
+export const VARIANT_SKIN_NAMES: Array<VariantSkinName> = [
+  // Zhaitan All
+  {
+    variantKey: 'zhaitanVariantSkins',
+    match: /^Zhaitan's (Rending|Claw|Tail|Argument|Wisdom|Fang|Gaze|Scale|Breath|Voice|Bite|Weight|Flight|Persuasion|Wing|Insight) Skin$/,
+  },
+]
+
+interface UApiItem {
+  id: number
+  name: string
+}
+
+const FILE_PATH = path.join(__dirname, './data.ts')
+const START_MARKER = `// -- GENERATE-START --`
+const END_MARKER = `// -- GENERATE-END --`
+const REPLACE_REGEX = new RegExp(`${escapeRegex(START_MARKER)}(.*)${escapeRegex(END_MARKER)}`, 's')
+
+async function run() {
+  const file = fs.readFileSync(FILE_PATH, 'utf-8')
+
+  const response = await fetch('https://api.gw2efficiency.com/items?lang=en&ids=all')
+  const items: Array<UApiItem> = await response.json()
+
+  let generatedItems = ''
+
+  VARIANT_SKIN_NAMES.forEach((variant, variantIndex) => {
+    const matchingItems = items.filter((item) => item.name.match(variant.match))
+
+    if (matchingItems.length === 0) {
+      console.error(`Could not find any items for '${variant.variantKey}'`)
+      process.exit(1)
+    }
+
+    matchingItems.sort((a, b) => a.id - b.id)
+
+    matchingItems.forEach((item, itemIndex) => {
+      const itemLine = [
+        `  { `,
+        `variant_key: '${variant.variantKey}', `,
+        `id: ${item.id}, `,
+        `name: '${escapeQuotes(item.name)}'`,
+        ` }`,
+      ]
+
+      generatedItems += itemLine.join('') + ','
+      if (
+        variantIndex !== VARIANT_SKIN_NAMES.length - 1 ||
+        itemIndex !== matchingItems.length - 1
+      ) {
+        generatedItems += '\n'
+      }
+    })
+
+    if (variantIndex !== VARIANT_SKIN_NAMES.length - 1) generatedItems += '\n'
+  })
+
+  const content = file.replace(REPLACE_REGEX, `${START_MARKER}\n${generatedItems}\n  ${END_MARKER}`)
+  fs.writeFileSync(FILE_PATH, content, 'utf-8')
+}
+
+run()
