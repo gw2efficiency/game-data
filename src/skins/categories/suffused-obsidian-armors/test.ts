1a3d4e5bddb8d9ef73d2d1f0ed2f377adcc65efe
Ecmelt <satanistanbul@hotmail.com>
2024-07-12 18:47:49 +0000

    format

--- src/skins/categories/suffused-obsidian-armors/test.ts
+++ src/skins/categories/suffused-obsidian-armors/test.ts
@@ -2,9 +2,9 @@ import { duplicates, unique } from 'flocky'
 import data from './data'
 
 it('does not have duplicated ids', () => {
-    const ids = duplicates(data.map((skin) => skin.id))
+  const ids = duplicates(data.map((skin) => skin.id))
 
-    if (ids.length > 0) {
-        throw new Error('Duplicated ids: ' + ids.join(', '))
-    }
+  if (ids.length > 0) {
+    throw new Error('Duplicated ids: ' + ids.join(', '))
+  }
 })
